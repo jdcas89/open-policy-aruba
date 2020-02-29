968d90ef671695747f72be8aa0364eaa055c48e6
Juan Castellanos <juan.castellanos@frontmen.nl>
2020-02-29 14:02:14 +0100

    New changelog

--- src/components/templates/Changelog/index.tsx
+++ src/components/templates/Changelog/index.tsx
@@ -10,15 +10,28 @@ const Changelog = () => {
       </H1>
       <VersionSection>
         <H2 p="16px" m="0" color="primary" fontSize="18px">
-          Version 0.1
+          Version 0.2.0
+        </H2>
+        <ul>
+          <li>Changelog & versioning</li>
+          <li>Motions per representative</li>
+          <li>First version of new motion request </li>
+          <li>Search in representatives</li>
+          <li>Footer improvements</li>
+          <li> Social section for representatives</li>
+        </ul>
+      </VersionSection>
+      <VersionSection>
+        <H2 p="16px" m="0" color="primary" fontSize="18px">
+          Version 0.1.0
         </H2>
         <ul>
           <li>Project setup </li>
-          <li>Leden lijst </li>
-          <li>Motie lijst</li>
-          <li>Motie Detail</li>
-          <li>Leden detail</li>
-          <li> New Motie pagina ( eerste versie )</li>
+          <li>Representative list </li>
+          <li>Motion list</li>
+          <li>Motion Detail</li>
+          <li>Representative detail</li>
+          <li> New Motion page ( eerste versie )</li>
         </ul>
       </VersionSection>
     </ChangeLogContainer>
